63ed171214168ae9dc159df789342e706a25a5f9
Nikaydo <sooosooor49@gmail.com>
2025-06-09 19:29:29 +0500

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"main/internal/config"
 	"main/internal/database"
@@ -15,32 +16,32 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":50052")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	grpcServer := grpc.NewServer()
 	env, err := config.ReadEnv()
 	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+		log.Fatal("Error loading auth .env file:", err)
 	}
-	log.Println("Database succesful read")
+	log.Println("Database auth succesful read")
 	db, err := database.DatabaseInit(env)
 	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+		log.Fatal("Error loading auth .env file:", err)
 	}
-	log.Println("Database succesful connected")
+	log.Println("Database auth succesful connected")
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%s", env.EnvMap["HOST"], env.EnvMap["PORT"]))
+	if err != nil {
+		log.Fatalf("failed to listen auth: %v", err)
+	}
+	grpcServer := grpc.NewServer()
 	auth.RegisterAuthServer(grpcServer, &au.AuthService{User: db})
-	log.Println("gRPC server started on :50052")
+	log.Println("gRPC auth server started on ", fmt.Sprintf("%s:%s", env.EnvMap["HOST"], env.EnvMap["PORT"]))
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
-			log.Fatalf("failed to serve: %v", err)
+			log.Fatalf("failed to serve auth: %v", err)
 		}
 	}()
 	<-quit
-	log.Println("Shutting down server...")
+	log.Println("Shutting down auth server...")
 	grpcServer.GracefulStop()
-	log.Println("Server gracefully stopped")
+	log.Println("Server auth gracefully stopped")
 }
